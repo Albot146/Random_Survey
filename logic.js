08818bb5fbc591a0403453cdd3de8b4a5407b8a2
Albot146 <albin.ottosson2002@gmail.com>
2025-05-22 13:28:32 +0200

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,12 @@
+
+
+x = Math.random();   
+
+if (x <= 1/3) {
+window.location.replace("https://google.com");
+} else if (x <= 2/3){
+window.location.replace("https://yahoo.com");
+}
+else if  (x > 2/3) {
+window.location.replace("https://bing.com");
+}
