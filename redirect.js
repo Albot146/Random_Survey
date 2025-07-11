f2c826c9b8633579fb3f4caa015b4cba49749116
Albot146 <albin.ottosson2002@gmail.com>
2025-07-11 10:52:03 +0200

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -3,7 +3,7 @@
 x = Math.random();   
 
 if (x <= 1/3) {
-window.location.replace("https://forms.gle/CpKFGqqvjLqrELTD8");
+window.location.replace("https://forms.gle/6ibvX8d8JacdL4rV7");
 } else if (x <= 2/3){
 window.location.replace("https://forms.gle/LkkgCfapwUqMR6pf9");
 }
